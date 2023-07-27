36dd4e9f277bbea74020dafc7b0d899ad45cc71e
IICAOCII <anwar.cemh.secu@gmail.com>
2023-07-27 09:39:51 -0600

    funcion hora

--- FuncionesPDF.py
+++ FuncionesPDF.py
@@ -1,16 +1,23 @@
 from reportlab.pdfgen import canvas
-
-
+from FuncionesQR import *
+import datetime
+import locale
+locale.setlocale(locale.LC_ALL,'')
 ruta = "C:/Users/Lenovo/Desktop/prueba funciones/prubea funciones/"
-nombreArchivo = ruta + "reporteGlobal.pdf"
-
+nombreQR = ruta + "miQR.png"
 def generarPDF(listaNombres, listaEdades):
+    fecha_actual = datetime.datetime.now()
+    nombreArchivo = ruta + "reporteGlobal_"+fecha_actual.strftime('%d_%m_%y_%H_%M_%S')+".pdf"
+    generarQR(nombreQR, "hola desde funcion")
     c = canvas.Canvas(nombreArchivo)
     xInicial = 200
     yInicial = 700
     c.setFont('Helvetica',20)
     c.drawString(xInicial,yInicial,"edad")
     c.drawString(xInicial + 120 ,yInicial,"nombre")
+    
+    c.drawImage(nombreQR,200,400,100,100)
+
     for i in range(len(listaNombres)):
         c.setFont('Helvetica',18)
         yInicial = yInicial -20 
